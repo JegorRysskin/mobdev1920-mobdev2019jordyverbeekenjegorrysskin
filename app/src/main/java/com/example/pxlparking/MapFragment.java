c3459a2a73a17d3fda6e199fc42dbd29cc823dac
JegorRysskin <jegorrysskin@gmail.com>
2020-01-10 22:31:16 +0100

    Remove ununused dependency

--- app/src/main/java/com/example/pxlparking/MapFragment.java
+++ app/src/main/java/com/example/pxlparking/MapFragment.java
@@ -10,7 +10,6 @@ import androidx.fragment.app.Fragment;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
