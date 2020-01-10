e09bfaffbdb5fa8f29cd7fc343d94423307f9335
JegorRysskin <jegorrysskin@gmail.com>
2020-01-10 22:30:22 +0100

    Removed unused code in mapfragment

--- app/src/main/java/com/example/pxlparking/MapFragment.java
+++ app/src/main/java/com/example/pxlparking/MapFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
@@ -13,7 +12,6 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -31,12 +29,6 @@ public class MapFragment extends Fragment {
 
         mMapView = view.findViewById(R.id.map_fragment);
         mMapView.onCreate(savedInstanceState);
-
-        try {
-            MapsInitializer.initialize(getActivity().getApplicationContext());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
 
         Bundle bundle = getArguments();
 
