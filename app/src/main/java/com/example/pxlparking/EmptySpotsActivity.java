84e7b0bc5b03c13ccb87b5b41552038d9052821a
JegorRysskin <jegorrysskin@gmail.com>
2020-01-10 21:44:19 +0100

    Now saves clicked favorite in localstorage.

--- app/src/main/java/com/example/pxlparking/EmptySpotsActivity.java
+++ app/src/main/java/com/example/pxlparking/EmptySpotsActivity.java
@@ -8,11 +8,15 @@ import androidx.core.app.NotificationManagerCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.app.Notification;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.database.DataSnapshot;
@@ -24,6 +28,13 @@ import com.google.firebase.database.ValueEventListener;
 import static com.example.pxlparking.App.CHANNEL_1_ID;
 
 public class EmptySpotsActivity extends AppCompatActivity {
+
+    private ToggleButton favoriteButton0;
+    private ToggleButton favoriteButton1;
+    private ToggleButton favoriteButton2;
+    private ToggleButton favoriteButton3;
+    private ToggleButton favoriteButton4;
+    private ToggleButton favoriteButton5;
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
@@ -40,6 +51,8 @@ public class EmptySpotsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_empty_spots);
 
         notificationManager = NotificationManagerCompat.from(this);
+
+        getToggleButtonsFromLocalStorage();
 
         mDrawerLayout = findViewById(R.id.drawerLayout);
         mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
@@ -125,7 +138,29 @@ public class EmptySpotsActivity extends AppCompatActivity {
             }
         });
 
+        setToggleButtonsToLocalStorage();
+    }
 
+    private void getToggleButtonsFromLocalStorage() { for (int i = 0; i <= 5; i++){
+        ToggleButton toggle = findViewById(getResources().getIdentifier("button_favorite" + i, "id", getPackageName()));
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        boolean defaultValue = getResources().getBoolean(R.bool.saved_isChecked_default_key);
+        boolean isChecked = sharedPref.getBoolean(toggle.getId() + "", defaultValue);
+        toggle.setChecked(isChecked);
+    } }
+
+    private void setToggleButtonsToLocalStorage() { for (int i = 0; i <= 5; i++){
+            final ToggleButton toggle = findViewById(getResources().getIdentifier("button_favorite" + i, "id", getPackageName()));
+            toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPref.edit();
+                    editor.putBoolean(toggle.getId() + "", isChecked);
+                    editor.apply();
+                }
+            });
+
+        }
     }
 
     @Override
